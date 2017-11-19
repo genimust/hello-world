a76137d6e60f84fa2539cba12229c92a34b845f9
mustapha <mustapha.bedjaoui@gmail.com>
2017-11-19 17:52:15 +0100

    update readme

---
+++ pack1/ob1.py
@@ -0,0 +1,4 @@
+def table_multi(a):
+    print "Table de multiplication de : ",a
+    for i in range(1,10):
+        print i,"*",a," = ", i*a
---
+++ test.py
@@ -0,0 +1,2 @@
+from  pack1.ob1 import table_multi
+table_multi(5)
