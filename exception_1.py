a13b9b6870ab721a221cb6358d027bc415270ea8
mustapha <mustapha.bedjaoui@gmail.com>
2017-12-02 18:26:47 +0100

    update readme

---
+++ exception_1.py
@@ -0,0 +1,7 @@
+while True:
+ try:
+  annee = input () # On demande l ' utilisateur de saisir
+  annee = int ( annee ) # On essaye de convertir l ' ann  en un entier
+  break
+ except:
+    print "feel the correct data"
