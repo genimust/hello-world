1afde046a830a117de480a84c9492b43ca6e8270
genimust <mustapha.bedjaoui@gmail.com>
2018-07-16 11:09:17 +0100

    problem 5

--- bfor.py
+++ bfor.py
@@ -1,6 +1,19 @@
-for i in "00456" :
-    print i
+#for i in "00456":
+#   print i
+import gc
+i=1L
+j=1
+print "start"
+for i in range( 1, 999999999):
+    j=1
+    while j<=11:
+        if j==11:
+            print "bingo",i
+            exit()
+        if (i % j !=0):
+           break
+        gc.collect()
+        j=j+1
 
-for j in range(1,10,2):
-    print j
+
 
